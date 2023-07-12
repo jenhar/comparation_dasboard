fc679d13b527188a0d6c16450aa4ae24ab51635a
jenhar <jenhar.hidayat@gmail.com>
2023-07-12 11:08:26 +0700

    set page rev

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 import plotly.express as px
 
-st.set_page_config(page_title='RECOMENDATION DASBOARD',
+st.set_page_config(page_title='COMPARATION DASBOARD',
                    page_icon=":bar_chart:",
                    layout="wide")
 
